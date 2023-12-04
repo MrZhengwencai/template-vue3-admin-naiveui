2e2fce6535f5c89255b9fa30f4e4ffcfb450a078
zwc <6537397+uni-yunApp@user.noreply.gitee.com>
2023-12-04 10:32:24 +0800

    feat:添加代码提交校验

---
+++ .husky/_/husky.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+###
+ # @Author: zwc 6537397+uni-yunApp@user.noreply.gitee.com
+ # @Date: 2023-12-04 09:52:54
+ # @LastEditors: zwc 6537397+uni-yunApp@user.noreply.gitee.com
+ # @LastEditTime: 2023-12-04 09:53:05
+ # @FilePath: \template-vue3-admin-naiveui\.husky\_\husky.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    if [ "$HUSKY_DEBUG" = "1" ]; then
+      echo "husky (debug) - $1"
+    fi
+  }
+
+  readonly hook_name="$(basename -- "$0")"
+  debug "starting $hook_name..."
+
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+
+  readonly husky_skip_init=1
+  export husky_skip_init
+  sh -e "$0" "$@"
+  exitCode="$?"
+
+  if [ $exitCode != 0 ]; then
+    echo "husky - $hook_name hook exited with code $exitCode (error)"
+  fi
+
+  if [ $exitCode = 127 ]; then
+    echo "husky - command not found in PATH=$PATH"
+  fi
+
+  exit $exitCode
+fi
